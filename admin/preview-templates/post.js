e2d601809fbc0c56594be3f90b6800fd9475f2d1
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-05-01 17:01:33 -0600

    editing preview for cms

--- admin/preview-templates/post.js
+++ admin/preview-templates/post.js
@@ -9,52 +9,38 @@ const Post = createClass({
     const entry = this.props.entry;
 
     return html`
-    <section id="litters">
-  <div class="litter-container">
-    <div class="picture">
-      <img
-        src="{{page.thumbnail | relative_url}}"
-        alt="{{ page.info | relative_url }}"
-        srcset=""
-      />
-    </div>
-    <div class="details">
-      
-      <div class="upcoming-litter container-text">
-        <h1 class="header">${entry.getIn(["data", "title"], null)}</h1>
-        <h2>${entry.getIn(["data", "color"], "")}</h2>
+      <main>
+        <article>
+          <h1>${entry.getIn(["data", "title"], null)}</h1>
+          <p>
+            <small>
+              <time
+                >${
+                  format(
+                    entry.getIn(["data", "date"], new Date()),
+                    "dd MMM, yyyy"
+                  )
+                }</time
+              >
+              ${" by Author"}
+            </small>
+          </p>
 
-        ${this.props.widgetFor("body")}
-        <p>
-          ${
-            entry.getIn(["data", "tags"], []).map(
-              tag =>
-                html`
-                  <a href="#" rel="tag">${tag}</a>
-                `
-            )
-          }
-        </p>
-        <h2>{{ page.color }}</h2>
-        <div class="upcoming-litter-text">
-          <h3 class="upcoming-litter-subheader">Kittens' Desc:</h3>
-          <p class="desc">{{ content }}</p>
-        </div>
-      </div>
-      <div class="upcoming-litter-text-container">
-        <h4 class="reserved">Reserved: {{page.reserved }}</h4>
-        <a href="{{ site.baseurl }}{% link pages/purchasing.md %}"
-          >Click here to Reserve</a
-        >
-      </div>
-    </div>
-    <div class="arrival">
-      <h4 class="arrival-text">
-        <p class="upcoming-litter">Expected: {{page.expected }}</p>
-      </h4>
-    </div>
-  </div>
-</section>
+          <p>${entry.getIn(["data", "summary"], "")}</p>
+
+          ${this.props.widgetFor("body")}
+          <p>
+            ${
+              entry.getIn(["data", "tags"], []).map(
+                tag =>
+                  html`
+                    <a href="#" rel="tag">${tag}</a>
+                  `
+              )
+            }
+          </p>
+        </article>
+      </main>
     `;
   }
 });
