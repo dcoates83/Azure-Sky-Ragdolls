29e702c4675f349e621de6e1ef6c39b9bd2ec75a
Devon <devon.coates@totaleto.com>
2022-10-13 17:55:12 -0600

    added sass

--- azureSky/web/src/App.tsx
+++ azureSky/web/src/App.tsx
@@ -1,11 +1,10 @@
 import { CssBaseline } from '@mui/material'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import './index.css'
+import '../src/_sass/output.css'
 
 const App = () => (
   //@ts-ignore
